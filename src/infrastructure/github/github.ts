4b256e28990bce8208819279d031a5630c8ed0cc
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2025-03-23 10:26:22 -0300

    Added error handling on github data fetch function

--- src/infrastructure/github/github.ts
+++ src/infrastructure/github/github.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CONFIG } from "../config/environment";
 
 export type UserData = {
@@ -18,26 +18,44 @@ export type UserData = {
   }[];
 };
 
-export const getUserData = async (username: string): Promise<UserData> => {
-  const userDataUrl = `https://api.github.com/users/${username}`;
-  const userRepositoriesUrl = `https://api.github.com/users/${username}/repos`;
+export const getUserData = async (
+  username: string
+): Promise<UserData | undefined> => {
+  try {
+    const userDataUrl = `https://api.github.com/users/${username}`;
+    const userRepositoriesUrl = `https://api.github.com/users/${username}/repos`;
 
-  const headers = {
-    Accept: "application/vnd.github+json",
-    "X-GitHub-Api-Version": "2022-11-28",
-    Authorization: `Bearer ${CONFIG.GITHUB_API_TOKEN}`,
-  };
+    const headers = {
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+      Authorization: `Bearer ${CONFIG.GITHUB_API_TOKEN}`,
+    };
 
-  const [userDataResponse, userReposResponse] = await Promise.all([
-    axios.get(userDataUrl, { headers }),
-    axios.get(userRepositoriesUrl, { headers }),
-  ]);
+    const [userDataResponse, userReposResponse] = await Promise.all([
+      axios.get(userDataUrl, { headers }),
+      axios.get(userRepositoriesUrl, { headers }),
+    ]);
 
-  const userData = userDataResponse.data;
-  const userRepos = userReposResponse.data;
+    const userData = userDataResponse.data;
+    const userRepos = userReposResponse.data;
 
-  return {
-    plainData: userData,
-    repositories: userRepos,
-  };
+    return {
+      plainData: userData,
+      repositories: userRepos,
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.status === 401)
+        console.error(`Error: ${error.status}: Unauthorized credentials`);
+      if (error.status === 403)
+        console.error(`Error: ${error.status}: Forbidden access`);
+      if (error.status === 404)
+        console.error(
+          `Error: ${error.status} - User with username ${username} not found.`
+        );
+      return undefined;
+    }
+    console.error("Some error has occurred while fetching user data", error);
+    return undefined;
+  }
 };
